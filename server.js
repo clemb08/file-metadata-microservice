9f4feb462b317003eca793ea89bf178995402a43
Glitch (separate-gabby-jonquil) <none>
2020-07-02 10:05:12 +0000

    👳🏝 Checkpoint ./server.js:18259900/377

--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+const multer = require('multer');
 
 var app = express();
 
+var storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, file)
+  },
+  filename: (req, file, done) =>{
+    done(null, file.fieldname)
+  }
+});
+
+var upload = multer({ storage : storage }).
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,6 +26,10 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post("/api/fileanalyse", (req, res) => {
+  
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
